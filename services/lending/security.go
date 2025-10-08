4067b0f9eade6a52979e8f5f556e1089aa089635
josephblackelite <99123409+josephblackelite@users.noreply.github.com>
2025-10-07 20:15:24 -0700

    Add TLS helper and dev docs for lending service

--- services/lending/security.go
+++ services/lending/security.go
@@ -2,11 +2,67 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"os"
+	"strings"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 
 	"nhbchain/network"
 )
+
+func buildServerCredentials(certPath, keyPath, clientCAPath string, mtlsRequired bool) (credentials.TransportCredentials, bool, error) {
+	certPath = strings.TrimSpace(certPath)
+	keyPath = strings.TrimSpace(keyPath)
+	clientCAPath = strings.TrimSpace(clientCAPath)
+
+	if certPath == "" || keyPath == "" {
+		return nil, false, fmt.Errorf("server certificate and key are required")
+	}
+
+	cert, err := tls.LoadX509KeyPair(certPath, keyPath)
+	if err != nil {
+		return nil, false, fmt.Errorf("load server certificate: %w", err)
+	}
+
+	tlsCfg := &tls.Config{
+		MinVersion:   tls.VersionTLS12,
+		Certificates: []tls.Certificate{cert},
+	}
+
+	var clientPool *x509.CertPool
+	if clientCAPath != "" {
+		pem, err := os.ReadFile(clientCAPath)
+		if err != nil {
+			return nil, false, fmt.Errorf("read client ca: %w", err)
+		}
+		pool := x509.NewCertPool()
+		if !pool.AppendCertsFromPEM(pem) {
+			return nil, false, fmt.Errorf("parse client ca: invalid pem data")
+		}
+		clientPool = pool
+		tlsCfg.ClientCAs = pool
+	}
+
+	mtlsEnabled := false
+	switch {
+	case mtlsRequired:
+		if clientPool == nil {
+			return nil, false, fmt.Errorf("client ca bundle required for mtls")
+		}
+		tlsCfg.ClientAuth = tls.RequireAndVerifyClientCert
+		mtlsEnabled = true
+	case clientPool != nil:
+		tlsCfg.ClientAuth = tls.VerifyClientCertIfGiven
+	default:
+		tlsCfg.ClientAuth = tls.NoClientCert
+	}
+
+	return credentials.NewTLS(tlsCfg), mtlsEnabled, nil
+}
 
 func newAuthInterceptors(auth network.Authenticator) (grpc.UnaryServerInterceptor, grpc.StreamServerInterceptor) {
 	unary := func(ctx context.Context, req interface{}, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
